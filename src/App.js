ea10e350698bbf6adaed54e5a5634ede6bc9f4f2
박신영 <sin5015243@naver.com>
2022-03-02 10:57:42 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  
+  let [title, titleChange] = useState('react 공부 시작');
+
+  let posts = '개발 시좍'
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style={{color: 'white', fontSize: '15px'}}>개발 Blog</div>
+      </div>
+      <div className="list">
+        <h3>{title}</h3>
+        <p>3월 1일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
